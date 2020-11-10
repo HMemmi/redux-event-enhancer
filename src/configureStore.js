f2e5bf0af9bf540c363911792376a07e6e079517
Hichem Memmi <hichem.memmi@outlook.com>
2020-11-10 01:12:49 +0100

    removed a useless return

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,14 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import createEventEnhancer from "./eventEnhancer";
 import todoReducer from "./reducer";
 
-const configStore = () => {
-  return configureStore({
+const configStore = () =>
+  configureStore({
     reducer: todoReducer,
     enhancers: [createEventEnhancer()],
     preloadedState: [
       { name: "watch anime", description: "watch wano arc !!!one piece!!!" }
     ]
   });
-};
 
 export default configStore;
